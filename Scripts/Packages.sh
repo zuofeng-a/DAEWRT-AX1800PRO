4715af143d5a848b4e34225a627c6a79ae4abf21
GHNERCH <166847742+GHNERCH@users.noreply.github.com>
2025-11-25 01:50:24 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -64,7 +64,7 @@ UPDATE_PACKAGE() {
 # UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 # UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 # UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master"
+# UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master"
 UPDATE_PACKAGE "luci-app-dae" "QiuSimons/luci-app-dae" "next"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
