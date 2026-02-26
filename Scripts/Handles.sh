fee8c54a87ab63dfad60f08075047498f9f015a2
zuofeng-a <zuofeng1974@gmail.com>
2026-02-26 23:54:21 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -3,26 +3,26 @@
 PKG_PATH="$GITHUB_WORKSPACE/$WRT_DIR/package/"
 
 #预置HomeProxy数据
-# if [ -d *"homeproxy"* ]; then
-# 	echo " "
+ if [ -d *"homeproxy"* ]; then
+ 	echo " "
 
-# 	HP_RULE="surge"
-# 	HP_PATH="homeproxy/root/etc/homeproxy"
+ 	HP_RULE="surge"
+ 	HP_PATH="homeproxy/root/etc/homeproxy"
 
-# 	rm -rf ./$HP_PATH/resources/*
+ 	rm -rf ./$HP_PATH/resources/*
 
-# 	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
-# 	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+ 	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
+ 	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
 
-# 	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-# 	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-# 	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-# 	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
+ 	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+ 	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+ 	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+ 	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
 
-# 	cd .. && rm -rf ./$HP_RULE/
+ 	cd .. && rm -rf ./$HP_RULE/
 
-# 	cd $PKG_PATH && echo "homeproxy date has been updated!"
-# fi
+ 	cd $PKG_PATH && echo "homeproxy date has been updated!"
+ fi
 
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
